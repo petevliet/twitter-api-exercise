62ccddea6d330f1b8aa98407e612acfce4604515
Peter Vliet <petervliet@Peters-MacBook-Air.local>
2015-03-10 13:47:13 -0600

    add tweetgrabber model and display tweets on welcome index

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,8 @@
 class WelcomeController < ApplicationController
 
   def index
-
+    @tweetget = TwitterGrabber.new
+    @mytweets = @tweetget.my_tweets
   end
 
 end
---
+++ app/models/twitter_grabber.rb
@@ -0,0 +1,16 @@
+class TwitterGrabber
+
+  def initialize
+    @client = Twitter::REST::Client.new do |config|
+      config.consumer_key        = Rails.application.secrets.consumer_key
+      config.consumer_secret     = Rails.application.secrets.consumer_secret
+      config.access_token        = Rails.application.secrets.access_token
+      config.access_token_secret = Rails.application.secrets.access_token_secret
+    end
+  end
+
+  def my_tweets
+    @client.user_timeline("Pete_Vliet")
+  end
+
+end
