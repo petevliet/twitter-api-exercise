04286790b868f2ea04b4770a2d65a750e8756b7f
Peter Vliet <petervliet@Peters-MacBook-Air.local>
2015-03-10 15:33:05 -0600

    users can look up tweeters, post tweets, and see my friends tweets

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,6 +3,12 @@ class WelcomeController < ApplicationController
   def index
     @tweetget = TwitterGrabber.new
     @mytweets = @tweetget.my_tweets
+    @findtweets = @tweetget.look_up_tweets(params[:username])
+    @my_buddies = @tweetget.my_followers
+
+    if params.has_key?(:message)
+      @post_tweet = @tweetget.post_tweet(params[:message])
+    end
   end
 
 end
--- app/models/twitter_grabber.rb
+++ app/models/twitter_grabber.rb
@@ -13,4 +13,16 @@ class TwitterGrabber
     @client.user_timeline("Pete_Vliet")
   end
 
+  def my_followers
+    @client.friends.map {|f| @client.user_timeline(f)}
+  end
+
+  def look_up_tweets(username)
+    @client.user_timeline(username)
+  end
+
+  def post_tweet(message)
+    @client.update(message)
+  end
+
 end
